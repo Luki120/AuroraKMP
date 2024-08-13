79fbbcaf5ab0f6a1045d5632fc6ea0221e608960
luki120 <luki120@exerra.xyz>
2024-08-13 06:24:02 -0300

    not needed

--- composeApp/src/commonMain/kotlin/ui/theme/Font.kt
+++ composeApp/src/commonMain/kotlin/ui/theme/Font.kt
@@ -9,10 +9,6 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun Typography(): Typography {
     return Typography(
-        bodySmall = TextStyle(
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Normal
-        ),
         labelSmall = TextStyle(
             fontSize = 14.sp,
             fontWeight = FontWeight.Normal
