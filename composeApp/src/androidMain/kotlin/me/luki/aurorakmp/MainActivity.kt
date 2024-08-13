99850ef6552a157414033667298a2918ea484490
luki120 <luki120@exerra.xyz>
2024-08-13 06:23:40 -0300

    .

--- composeApp/src/androidMain/kotlin/me/luki/aurorakmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/me/luki/aurorakmp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.remember
 import di.PasswordGeneratorProvider.providePasswordGeneratorRepository
 import di.SettingsProvider.provideSettingsRepository
 import ui.presentation.App
