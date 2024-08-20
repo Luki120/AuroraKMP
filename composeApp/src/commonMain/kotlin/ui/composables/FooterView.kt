97f5a65a846edbb4da2ea05a4b4f90cbf7bea37a
luki120 <luki120@exerra.xyz>
2024-08-19 22:35:44 -0300

    [composeApp] footer improvements
    • replaced ClickableText with a normal Row with two Text objects so that we can show the hand cursor when hovering only on the clickable label

--- composeApp/src/commonMain/kotlin/ui/composables/FooterView.kt
+++ composeApp/src/commonMain/kotlin/ui/composables/FooterView.kt
@@ -1,11 +1,7 @@
 package ui.composables
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,9 +11,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import components.application
@@ -31,33 +24,29 @@ fun FooterView() {
 private fun AttributedFooterText(string: String, subString: String) {
     val uriHandler = LocalUriHandler.current
 
-    val annotatedString = buildAnnotatedString {
-        withStyle(style = SpanStyle(color = Color.Gray)) {
-            append(string)
-        }
-        pushStringAnnotation(tag = subString, annotation = "https://github.com/Luki120/AuroraKMP")
-        withStyle(style = SpanStyle(color = MaterialTheme.colorScheme.primary)) {
-            append(subString)
-        }
-        pop()
-    }
-
     Box(modifier = Modifier.padding(bottom = 30.dp)) {
         Column(
             modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
-            ClickableText(
-                annotatedString,
-                onClick = { offset ->
-                    annotatedString.getStringAnnotations(tag = "open source", start = offset, end = offset)
-                        .firstOrNull()?.let {
-                            uriHandler.openUri(it.item)
+            Row {
+                Text(
+                    text = string,
+                    color = Color.Gray,
+                    fontSize = MaterialTheme.typography.labelSmall.fontSize
+                )
+                Text(
+                    text = subString,
+                    color = MaterialTheme.colorScheme.primary,
+                    fontSize = MaterialTheme.typography.labelSmall.fontSize,
+                    modifier = Modifier
+                        .clickable {
+                            uriHandler.openUri("https://github.com/Luki120/AuroraKMP")
                         }
-                },
-                modifier = Modifier.pointerHoverIcon(PointerIcon.Hand)
-            )
+                        .pointerHoverIcon(PointerIcon.Hand)
+                )
+            }
             Text("© 2024 Luki120", fontSize = 10.sp, color = Color.Gray)
         }
     }
