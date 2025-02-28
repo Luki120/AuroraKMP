95a871b308315a07af5f3354e428a42e18745444
luki120 <luki120@exerra.xyz>
2025-02-28 02:25:11 -0300

    [composeApp] refactored `AppViewModel` with simplified logic
    skip-checks: true

--- composeApp/src/commonMain/kotlin/ui/presentation/AppViewModel.kt
+++ composeApp/src/commonMain/kotlin/ui/presentation/AppViewModel.kt
@@ -4,12 +4,8 @@ import data.PasswordGeneratorRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
-import data.SettingsRepository
 
-class AppViewModel(
-    private val passwordGeneratorRepository: PasswordGeneratorRepository,
-    private val settingsRepository: SettingsRepository
-) {
+class AppViewModel(private val passwordGeneratorRepository: PasswordGeneratorRepository) {
     private var _passwordState = MutableStateFlow(PasswordState())
     val passwordState: StateFlow<PasswordState> = _passwordState
 
@@ -20,41 +16,25 @@ class AppViewModel(
     fun handlePasswordIntent(intent: PasswordIntent) {
         when (intent) {
             is PasswordIntent.IncludeUppercase -> {
-                settingsRepository.saveData(
-                    key = SettingsRepository.Constants.INCLUDE_UPPERCASE_KEY,
-                    value = intent.includeUppercase
-                )
                 _passwordState.update { it.copy(includeUppercase = intent.includeUppercase) }
                 passwordGeneratorRepository.includeUppercase = intent.includeUppercase
             }
             is PasswordIntent.IncludeNumbers -> {
-                settingsRepository.saveData(
-                    key = SettingsRepository.Constants.INCLUDE_NUMBERS_KEY,
-                    value = intent.includeNumbers
-                )
                 _passwordState.update { it.copy(includeNumbers = intent.includeNumbers) }
                 passwordGeneratorRepository.includeNumbers = intent.includeNumbers
             }
             is PasswordIntent.IncludeSymbols -> {
-                settingsRepository.saveData(
-                    key = SettingsRepository.Constants.INCLUDE_SYMBOLS_KEY,
-                    value = intent.includeSymbols
-                )
                 _passwordState.update { it.copy(includeSymbols = intent.includeSymbols) }
                 passwordGeneratorRepository.includeSymbols = intent.includeSymbols
             }
             is PasswordIntent.LaunchApp -> {
-                val includeUppercase = settingsRepository.retrieveData(key = SettingsRepository.Constants.INCLUDE_UPPERCASE_KEY)
-                val includeNumbers = settingsRepository.retrieveData(key = SettingsRepository.Constants.INCLUDE_NUMBERS_KEY)
-                val includeSymbols = settingsRepository.retrieveData(key = SettingsRepository.Constants.INCLUDE_SYMBOLS_KEY)
-
-                _passwordState.update { it.copy(includeUppercase = includeUppercase) }
-                _passwordState.update { it.copy(includeNumbers = includeNumbers) }
-                _passwordState.update { it.copy(includeSymbols = includeSymbols) }
-
-                passwordGeneratorRepository.includeUppercase = includeUppercase
-                passwordGeneratorRepository.includeNumbers = includeNumbers
-                passwordGeneratorRepository.includeSymbols = includeSymbols
+                _passwordState.update {
+                    it.copy(
+                        includeUppercase = passwordGeneratorRepository.includeUppercase,
+                        includeNumbers = passwordGeneratorRepository.includeNumbers,
+                        includeSymbols = passwordGeneratorRepository.includeSymbols
+                    )
+                }
             }
         }
     }
@@ -67,8 +47,8 @@ data class PasswordState(
 )
 
 sealed class PasswordIntent {
-    data class IncludeUppercase(val includeUppercase: Boolean): PasswordIntent()
-    data class IncludeNumbers(val includeNumbers: Boolean): PasswordIntent()
-    data class IncludeSymbols(val includeSymbols: Boolean): PasswordIntent()
-    data object LaunchApp: PasswordIntent()
+    data class IncludeUppercase(val includeUppercase: Boolean) : PasswordIntent()
+    data class IncludeNumbers(val includeNumbers: Boolean) : PasswordIntent()
+    data class IncludeSymbols(val includeSymbols: Boolean) : PasswordIntent()
+    data object LaunchApp : PasswordIntent()
 }
